4797ddb53b4432548b27d0b78ea2cfd9b3a7161f
Luis_Duarte <luise.duarte29@gmail.com>
2024-11-14 15:12:07 -0300

    Agregar archivos de proyecto.

---
+++ reactapp1.client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+
+
+    return (
+        <div>
+            <h1 id="tableLabel">Hola mundo nuevo</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+
+        </div>
+    );
+    
+
+}
+
+export default App;
